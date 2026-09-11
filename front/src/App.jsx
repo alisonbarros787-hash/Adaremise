3902440d410a54cc8fc5fac82eb9d0300f29b0f7
Aurélie Ichelmann <aurelie.ichelmann@gmail.com>
2026-09-11 11:14:02 +0200

    ajout route

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -18,12 +18,9 @@ function App() {
       <Route path="/" element={<Benevole/>}/>
       <Route path="/objets" element={<ListeObjets/>}/>
       <Route path="/objets/:id" element={<FicheObjet/>}/>
-<<<<<<< HEAD
       <Route path="/depots" element={<Depot/>}/>
       <Route path="/depots/:id" element={<UserBack/>}/>
-=======
       <Route path="/stats" element={<Stats/>}/>
->>>>>>> stats
     </Routes>
   )
 }
