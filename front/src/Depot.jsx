fb317737276f3ab17ab95bfb482138a788b9e9cc
Bernardo Sanchez Zapata <brnrdsz@gmail.com>
2026-09-14 12:32:20 +0200

    Changement route de personnes a benevoles

--- front/src/Depot.jsx
+++ front/src/Depot.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const API_URL = "http://localhost:3000/api/personnes";
+const API_URL = "http://localhost:3000/api/personnes/benevoles";
 const DEPOT_URL = "http://localhost:3000/api/depots";
 
 function Depot() {
